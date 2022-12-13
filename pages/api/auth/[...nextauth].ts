0afeae898e260637dea493185338e9c9a1981d49
Soc Sieng <socsieng@gmail.com>
2022-12-13 10:40:44 +0000

    chore: Remove email provider from auth options

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,9 +1,7 @@
 import { NextApiHandler } from "next";
 import NextAuth from "next-auth";
-import Providers from "next-auth/providers";
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import GitHubProvider from 'next-auth/providers/github'
-import EmailProvider from 'next-auth/providers/email'
 import prisma from '../../../lib/prisma'
 
 
@@ -15,17 +13,6 @@ const options = {
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-    }),
-    EmailProvider({
-      server: {
-        host: process.env.SMTP_HOST,
-        port: Number(process.env.SMTP_PORT),
-        auth: {
-          user: process.env.SMTP_USER,
-          pass: process.env.SMTP_PASSWORD,
-        },
-      },
-      from: process.env.SMTP_FROM,
     }),
   ],
   adapter: PrismaAdapter(prisma),
