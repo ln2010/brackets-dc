beef6b4c4d13129611f52b729f14fa64f043c6ea
Lloyd Nanka <lloyd.nanka@x15.com.au>
2022-12-15 21:07:11 +1100

    fix: improve docker db edge cases

--- scripts/db-up.sh
+++ scripts/db-up.sh
@@ -1,13 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 # Set the name of the container
 container_name="my_postgres_db"
 
-# Check if the container is already running
-if [ $(docker inspect -f '{{.State.Running}}' "$container_name") == "true" ]; then
-  # Container is running, start it
-  docker start "$container_name"
-else
-  # Container is not running, run it
+# Check if the container exists
+if [[ $(docker inspect --format='{{.State.Running}}' "$container_name") == "" ]]; then
+  # Container does not exist, create it
   docker run -d -p 5432:5432 --hostname db -e POSTGRES_DB=my_database -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres  --name "$container_name" postgres:15
+else
+  # Container exists, check if it is running
+  if [[ $(docker inspect --format='{{.State.Running}}' "$container_name") == "true" ]]; then
+    # Container is running, do nothing
+    :
+  else
+    # Container is not running, start it
+    docker start "$container_name"
+  fi
 fi
